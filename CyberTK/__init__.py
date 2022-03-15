2ec0ceeb9eb1b0542fd287afade7916471d686f1
TKR <tolg@cybertkr.com>
2022-03-15 13:58:36 +0100

    *-*

--- CyberTK/__init__.py
+++ CyberTK/__init__.py
@@ -1,10 +1,6 @@
-import aLLTypes,Modulethrift,talkFuncs
-
-# ORIGINAL LICENSE
 __copyright__       = 'Copyright 2021-2022 CyberTK'
 __version__         = '1.0 - beta'
 __license__         = 'BSD 3-Clause'
 __author__          = 'CybeTK'
 __url__             = 'http://github.com/CyberTKR'
 
-__all__ = ['aLLTypes', 'Modulethrift', 'talkFuncs']
